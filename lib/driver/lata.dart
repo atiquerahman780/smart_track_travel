ae0516a22bbe57b475c36963a690e54d67496f7a
dell pc <atiquerahman780@gmail.com>
2022-08-13 12:57:57 +0500

    end

---
+++ lib/driver/lata.dart
@@ -0,0 +1,5 @@
+class Lata{
+  late String driverlocation,drivername,driverpicture,driveruid,parentname, parentphone, parentuid,startpoint,key1;
+
+  Lata(this.driverlocation,this.drivername,this.driverpicture,this.driveruid,this.parentname,this.parentphone, this.parentuid,this.startpoint,this.key1);
+}
---
+++ lib/driver/sata.dart
@@ -0,0 +1,7 @@
+class Sata{
+  late String driveruid ,endpoint,parentname,parentphone,parentuid, requiredseats,
+   startingpoint, drivername,Driverphone,driverlocation,driverpicture,key,index;
+
+
+  Sata(this.driveruid,this.endpoint,this.parentname,this.parentphone,this.parentuid,this.requiredseats,this.startingpoint,this.drivername,this.Driverphone,this.driverlocation,this.driverpicture,this.key,this.index);
+}
